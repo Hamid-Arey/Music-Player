aea048c205c216be3027c6567d4267db275728b5
Hamid-Arey <hamidarey71@gmail.com>
2025-02-22 22:08:57 -0500

    Initialized repository

---
+++ data.js
@@ -0,0 +1,479 @@
+const music_list = [
+    {
+        name: "Free Smoke",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/freeSmoke.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 1
+    },
+    /*{
+      name: "No Long Talk",
+      artist: "Drake",
+      feature: ", Giggs",
+      album: "More Life",
+      music: "media/music/noLongTalk.mp3",
+      img: "media/cover/moreLife.jpg",
+      num: 2
+    },*/
+    {
+        name: "Passionfruit",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/passionfruit.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 3
+    },
+    /*{
+      name: "Jorja Interlude",
+      artist: "Drake",
+      feature: " ",
+      album: "More Life",
+      music: "media/music/jorjaInterlude.mp3",
+      img: "media/cover/moreLife.jpg",
+      num: 4
+    },*/
+    {
+        name: "Get It Together",
+        artist: "Drake",
+        feature: ", Black Coffee, Jorja Smith",
+        album: "More Life",
+        music: "media/music/getItTogether.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 5
+    },
+    {
+        name: "Madiba Riddim",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/madibaRiddim.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 6
+    },
+    {
+        name: "Blem",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/blem.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 7
+    },
+    {
+        name: "4422",
+        artist: "Drake",
+        feature: ", Sampha",
+        album: "More Life",
+        music: "media/music/4422.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 8
+    },
+    {
+        name: "Gyalchester",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/gyalchester.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 9
+    },
+    {
+        name: "Skepta Interlude",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/skeptaInterlude.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 10
+    },
+    {
+        name: "Portland",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/portland.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 11
+    },
+    {
+        name: "Sacrifices",
+        artist: "Drake",
+        feature: ", 2 Chainz, Young Thug",
+        album: "More Life",
+        music: "media/music/sacrifices.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 12
+    },
+    {
+        name: "Nothings Into Somethings",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/nothingsIntoSomethings.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 13
+    },
+    {
+        name: "Teenage Fever",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/teenageFever.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 14
+    },
+    {
+        name: "KMT",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/kmt.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 15
+    },
+    {
+        name: "Lose You",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/loseYou.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 16
+    },
+    {
+        name: "Can't Have Everything",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/can'tHaveEverything.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 17
+    },
+    // {
+    //     name: "Glow",
+    //     artist: "Drake",
+    //     feature: ", Kanye West",
+    //     album: "More Life",
+    //     music: "media/music/glow.mp3",
+    //     img: "media/cover/moreLife.jpg",
+    //     num: 18
+    // },
+    // {
+    //     name: "Since Way Back",
+    //     artist: "Drake",
+    //     feature: ", PARTYNEXTDOOR",
+    //     album: "More Life",
+    //     music: "sinceWayBack.mp3",
+    //     img: "media/cover/moreLife.jpg",
+    //     num: 19
+    // },
+    {
+        name: "Fake Love",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/fakeLove.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 20
+    },
+    {
+        name: "Ice Melts",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/iceMelts.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 22
+    },
+    {
+        name: "Do Not Disturb",
+        artist: "Drake",
+        feature: " ",
+        album: "More Life",
+        music: "media/music/doNotDisturb.mp3",
+        img: "media/cover/moreLife.jpg",
+        num: 23
+    },
+    {
+        name: "Deep Pockets",
+        artist: "Drake",
+        feature: " ",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/deepPockets.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 24
+    },
+    {
+        name: "When To Say When",
+        artist: "Drake",
+        feature: " ",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/whenToSayWhen.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 25
+    },
+    {
+        name: "Chicago Freestyle",
+        artist: "Drake",
+        feature: ", Giveon",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/chicagoFreestyle.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 26
+    },
+    // {
+    //     name: "Not You Too",
+    //     artist: "Drake",
+    //     feature: ", Chris Brown",
+    //     album: "Dark Lane Demo Tapes",
+    //     music: "media/music/notYouToo.mp3",
+    //     img: "media/cover/darkLaneDemoTapes.jpg",
+    //     num: 27
+    // },
+    {
+        name: "Toosie Slide",
+        artist: "Drake",
+        feature: " ",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/toosieSlide.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 28
+    },
+    {
+        name: "Desires",
+        artist: "Drake",
+        feature: ", Future",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/desires.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 29
+    },
+    {
+        name: "Time Flies",
+        artist: "Drake",
+        feature: " ",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/timeFlies.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 30
+    },
+    {
+        name: "Landed",
+        artist: "Drake",
+        feature: " ",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/landed.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 31
+    },
+    {
+        name: "D4L",
+        artist: "Drake",
+        feature: ", Future, Young Thug",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/d4l.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 32
+    },
+    {
+        name: "Pain 1993",
+        artist: "Drake",
+        feature: ", Playboi Carti",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/pain1993.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 33
+    },
+    {
+        name: "Losses",
+        artist: "Drake",
+        feature: " ",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/losses.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 34
+    },
+    {
+        name: "From Florida With Love",
+        artist: "Drake",
+        feature: " ",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/fromFloridaWithLove.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 35
+    },
+    {
+        name: "Demons",
+        artist: "Drake",
+        feature: ", Favio Foreign, Sosa Greek",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/demons.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 36
+    },
+    {
+        name: "War",
+        artist: "Drake",
+        feature: " ",
+        album: "Dark Lane Demo Tapes",
+        music: "media/music/war.mp3",
+        img: "media/cover/darkLaneDemoTapes.jpg",
+        num: 37
+    },
+    {
+        name: "STARGAZING",
+        artist: "Travis Scott",
+        feature: " ",
+        album: "Astroworld",
+        music: "media/music/stargazing.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 38
+    },
+    {
+        name: "CAROUSEL",
+        artist: "Travis Scott",
+        feature: ", Frank Ocean",
+        album: "Astroworld",
+        music: "media/music/carousel.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 39
+    },
+    // {
+    //     name: "SICKO MODE",
+    //     artist: "Travis Scott",
+    //     feature: ", Drake",
+    //     album: "Astroworld",
+    //     music: "media/music/SickoMode.mp3",
+    //     img: "media/cover/astroworld.jpg",
+    //     num: 40
+    // },
+    // {
+    //     name: "R.I.P. SCREW",
+    //     artist: "Travis Scott",
+    //     feature: ", Swae Lee",
+    //     album: "Astroworld",
+    //     music: "media/music/ .mp3",
+    //     img: "media/cover/astroworld.jpg",
+    //     num: 41
+    // },
+    // {
+    //     name: "STOP TRYING TO BE GOD",
+    //     artist: "Travis Scott",
+    //     feature: ", James Blake, Kid Cudi, Stevie Wonder",
+    //     album: "Astroworld",
+    //     music: "media/music/stopDryingToBoGod.mp3",
+    //     img: "media/cover/astroworld.jpg",
+    //     num: 42
+    // },
+    {
+        name: "SKELETONS",
+        artist: "Travis Scott",
+        feature: ", The Weeknd",
+        album: "Astroworld",
+        music: "media/music/skeletons.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 43
+    },
+    {
+        name: "NO BYSTANDERS",
+        artist: "Travis Scott",
+        feature: ", Juice WRLD, Sheck Wes",
+        album: "Astroworld",
+        music: "media/music/noBystanders.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 44
+    },
+    {
+        name: "WAKE UP",
+        artist: "Travis Scott",
+        feature: ", The Weeknd",
+        album: "Astroworld",
+        music: "media/music/wakeUp.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 45
+    },
+    // {
+    //     name: "5% TINT",
+    //     artist: "Travis Scott",
+    //     feature: " ",
+    //     album: "Astroworld",
+    //     music: "media/music/5%tint.mp3",
+    //     img: "media/cover/astroworld.jpg",
+    //     num: 46
+    // },
+    {
+        name: "NC-17",
+        artist: "Travis Scott",
+        feature: ", 21 Savage",
+        album: "Astroworld",
+        music: "media/music/nc-17.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 47
+    },
+    {
+        name: "ASTROTHUNDER",
+        artist: "Travis Scott",
+        feature: " ",
+        album: "Astroworld",
+        music: "media/music/astrothunder.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 48
+    },
+    {
+        name: "YOSEMITE",
+        artist: "Travis Scott",
+        feature: ", Gunna, NAV",
+        album: "Astroworld",
+        music: "media/music/yosemite.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 49
+    },
+    {
+        name: "CAN'T SAY",
+        artist: "Travis Scott",
+        feature: ", Don Toliver",
+        album: "Astroworld",
+        music: "media/music/can'tSay.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 50
+    },
+    {
+        name: "WHO? WHAT!",
+        artist: "Travis Scott",
+        feature: ", Quavo, Takeoff",
+        album: "Astroworld",
+        music: "media/music/whoWhat.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 51
+    },
+    {
+        name: "BUTTERFLY EFFECT",
+        artist: "Travis Scott",
+        feature: " ",
+        album: "Astroworld",
+        music: "media/music/butterflyEffect.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 52
+    },
+    {
+        name: "HOUSTONFORNICATION",
+        artist: "Travis Scott",
+        feature: " ",
+        album: "Astroworld",
+        music: "media/music/houstonFornication.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 53
+    },
+    {
+        name: "COFFEE BEAN",
+        artist: "Travis Scott",
+        feature: " ",
+        album: "Astroworld",
+        music: "media/music/coffeeBean.mp3",
+        img: "media/cover/astroworld.jpg",
+        num: 54
+    }
+]
